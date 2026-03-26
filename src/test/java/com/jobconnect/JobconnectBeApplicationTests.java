693379c477ddb682d7b6c8d3fa4dee867738be62
duynhat04 <luannhat15@gmail.com>
2026-03-26 15:40:02 +0700

    Khởi tạo dự án

---
+++ src/test/java/com/jobconnect/JobconnectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobconnectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
